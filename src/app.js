de21e50ba641e9a57757f79bc5dd47610b9ed887
Ozan Kücük <ozan.kuecuek@ad.diva-e.com>
2024-10-07 17:41:41 +0200

    Update CORS configuration and add auth routes

--- src/app.js
+++ src/app.js
@@ -11,14 +11,10 @@ const port = process.env.PORT || 5000;
 const corsOrigins = process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : ['http://localhost:3000'];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || corsOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
+  origin: corsOrigins,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 
