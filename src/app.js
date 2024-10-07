aaef8723e8ad7c3a1557653e31d5db1bd8e564ef
Ozan Kücük <ozan.kuecuek@gmail.com>
2024-10-07 19:34:46 +0200

    cors

--- src/app.js
+++ src/app.js
@@ -8,16 +8,10 @@ const authRoutes = require('./routes/authRoutes');
 const app = express();
 const port = process.env.PORT || 5000;
 
-const corsOrigins = process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : ['http://localhost:3000', 'https://once-frontend.vercel.app'];
+const corsOrigins = process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : ['http://localhost:3000', 'https://once-frontend.vercel.app', 'https://once-frontend-git-development-ozans-projects-6669b5a2.vercel.app'];
 
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || corsOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: corsOrigins,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
